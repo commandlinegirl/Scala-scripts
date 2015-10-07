264b5a494d47dc35c3fc76adc0b583a85ad30abe
Aleksandra Zalcman <biokere@gmail.com>
2015-10-07 13:15:44 +0200

    Adding solution to problem P31

--- src/com/commandlinegirl/ninetynine/arithmetic/S99Int.scala
+++ src/com/commandlinegirl/ninetynine/arithmetic/S99Int.scala
@@ -6,9 +6,21 @@ package com.commandlinegirl.ninetynine.arithmetic
 
 class S99Int(val start: Int) {
   import S99Int._
+
+  def - (other: S99Int): S99Int =
+    this - other
+
+  def toInt: Int =
+    start
+
+  /* 31. Determine whether a given integer number is prime.*/
+  def isPrime(start: Int): Boolean = {
+    val r = 2 to math.sqrt(start).toInt
+    println(r)
+    !(r exists (start % _ == 0))
+  }
 }
 
 object S99Int {
   implicit def int2S99Int(i: Int): S99Int = new S99Int(i)
-}
 }
